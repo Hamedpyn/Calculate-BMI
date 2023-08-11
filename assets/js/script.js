999fe1f7cd974f975d1176ef1db2601169786b64
Hamed <hamedpyn@gmail.com>
2023-08-11 16:28:53 +0430

    select DOM elements in js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Select DOM Elements
+
+let
+    userWeight = document.querySelector('#weight'),
+    weightText = document.querySelector('#weight-val'),
+    userHeight = document.querySelector('#height'),
+    heightText = document.querySelector('#height-val'),
+    resultNumber = document.querySelector('#result'),
+    resultText = document.querySelector('#category');
