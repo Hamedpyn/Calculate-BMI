1d1937c1e37e0e6c939a5b22ffe06e9bbc2790d1
Hamed <hamedpyn@gmail.com>
2023-08-11 16:40:42 +0430

    change the heightText and weightText in a ms when the input range is changing

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,16 @@ let
     heightText = document.querySelector('#height-val'),
     resultNumber = document.querySelector('#result'),
     resultText = document.querySelector('#category');
+
+
+// change the heightText and weightText in a ms when the input range is changing
+function changInputValue() {
+    let height = userHeight.value
+    let weight = userWeight.value
+    heightText.innerHTML = `${height} cm`
+    weightText.innerHTML = `${weight} kg`
+}
+
+// call an event to change in second when the input value is changing
+userHeight.addEventListener('input', changInputValue)
+userWeight.addEventListener('input', changInputValue)
